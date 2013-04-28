7ac51b621b9b2ad540ea71caf9925e6fdae352cc
Daniel Damiani <dsdamiani@gmail.com>
2013-04-27 17:03:03 -0700

    Working version of project A

--- src/main/java/com/ddamiani/codejam/Tongues.java
+++ src/main/java/com/ddamiani/codejam/Tongues.java
@@ -7,31 +7,35 @@ import java.util.*;
  * Project A of google code jam 2012
  */
 public class Tongues {
-    private static void printOutputLine(int iteration, String result) {
+    private static String getFormattedOutputLine(int iteration, String result) {
         final String templateStr = "Case #%d: %s\n";
-        System.out.format(templateStr, iteration, result);
+        return String.format(templateStr, iteration, result);
     }
 
     public static void main(String[] args) {
-        Translation trans = new Translation(Tongues.class.getResourceAsStream("/normal_letters.txt"),
-                Tongues.class.getResourceAsStream("/mutated_letters.txt"));
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-        int num_line = 0;
-        List<String> lines = new ArrayList<String>();
-
-        try {
-            num_line = Integer.parseInt(reader.readLine());
-        } catch (IOException e) {
-            System.err.println("Can't read from standard in");
-            System.exit(1);
-        } catch (NumberFormatException ne) {
-            System.err.println("Invalid number of initial line: " + ne.getMessage());
+        if(args.length < 1) {
+            System.err.println("No input file specified!");
             System.exit(1);
         }
 
+        Translation trans = new Translation(Tongues.class.getResourceAsStream("/normal_letters.txt"),
+                Tongues.class.getResourceAsStream("/mutated_letters.txt"));
+
+        String inputFileName = args[0];
+        List<String> lines = new ArrayList<String>();
+
         try {
+            int num_line = 0;
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(inputFileName)));
+            try {
+                num_line = Integer.parseInt(reader.readLine());
+            } catch (NumberFormatException ne) {
+                reader.close();
+                System.err.println("Invalid number of initial line: " + ne.getMessage());
+                System.exit(1);
+            }
+
             for(int i=1; i<=num_line; i++) {
                 String line = reader.readLine();
 
@@ -42,15 +46,40 @@ public class Tongues {
 
                 lines.add(translatedStr.toString());
             }
-
             reader.close();
+        } catch (FileNotFoundException fe) {
+            System.err.println("Specified input file not found: " + inputFileName);
+            System.exit(1);
         } catch (IOException e) {
-            System.err.println("Failed to read the expected lines");
+            System.err.println("Can't read from standard in");
             System.exit(1);
         }
 
-        for(int i=0;i<lines.size();i++) {
-            printOutputLine(i, lines.get(i));
+
+        OutputStream outputStream = null;
+        if(args.length >= 2) {
+            String outputStreamName = args[1];
+            try {
+                outputStream = new FileOutputStream(outputStreamName);
+            } catch (FileNotFoundException e) {
+                System.err.println("Specified input file not found: " + inputFileName);
+                System.exit(1);
+            }
+        } else {
+            outputStream = System.out;
+        }
+
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream));
+
+        try {
+            for(int i=0;i<lines.size();i++) {
+                writer.write(getFormattedOutputLine(i+1, lines.get(i)));
+            }
+
+            writer.close();
+        } catch (IOException e) {
+            System.err.println("Problem writing to the output stream: " + e.getMessage());
+            System.exit(1);
         }
     }
 }
